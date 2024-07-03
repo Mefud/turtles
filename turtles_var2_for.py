ae58e9c2d58cfe3b2fed273c1bd39e17a2de2c4b
mef <mefud@mail.ru>
2024-07-03 12:42:54 +0500

    turtles version 2

---
+++ turtles_var2_for.py
@@ -0,0 +1,12 @@
+for x in 'Whois':
+   name=input('What is your name?\n')
+   if name =='stop': break
+   Whois={'Nikita': 'Yes, you are Leonardo!',   
+       'Egor': 'Eee boy! You - Donatello!',
+       'Pavel': 'Fucking shit! You are Raf!',
+       'Ivan': 'Yahoo!!!! You are Miky! KAVABANGA!!!'}
+   if name in Whois:
+      print(Whois[name])
+   else:
+      print('You are not a ninja turtle')
+      
